bbc79ed832bf3c6bf34a36899e0a669295f9b1fe
Neel Mehendale <144788333+neelSchool@users.noreply.github.com>
2025-02-01 21:20:53 +0000

    Homework done

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -6,13 +6,28 @@
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
 
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
+            // There is no readme file so my evidence will be here.
+            // My code is going to run in O(n) time.
+            // Because that is the maximum efficiency of linear search.
+            // I am not using an IDE, I am just coding on github itself.
+            Console.WriteLine("Enter fruit to find");
+            string FruitToFind = (Console.ReadLine()).ToLower();
+            bool found = false;
+            foreach (string s in fruits)
+            {
+                if (s == FruitToFind)
+                {
+                    found = true;
+                }
+            }
+            if (found == true)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
     }
 }
